5f37a472182da8385ed1626697b0daf72f92feac
rezaa89 <reza.asadi68@gmail.com>
2020-11-23 14:44:44 -0800

    Update linear_assignment.py

--- deep_sort/deep_sort/linear_assignment.py
+++ deep_sort/deep_sort/linear_assignment.py
@@ -186,5 +186,5 @@ def gate_cost_matrix(
         track = tracks[track_idx]
         gating_distance = kf.gating_distance(
             track.mean, track.covariance, measurements, only_position)
-        cost_matrix[row, gating_distance > gating_threshold] = gated_cost
+        #cost_matrix[row, gating_distance > gating_threshold] = gated_cost
     return cost_matrix
