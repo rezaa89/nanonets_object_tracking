d3b23eda42136c5ed16874982552d9a06899ea1e
rezaa89 <reza.asadi68@gmail.com>
2020-11-30 12:41:45 -0800

    Update kalman_filter.py

--- deep_sort/deep_sort/kalman_filter.py
+++ deep_sort/deep_sort/kalman_filter.py
@@ -10,9 +10,9 @@ function and used as Mahalanobis gating threshold.
 """
 chi2inv95 = {
     1: 3.8415,
-    2: 80,
+    2: 9,
     3: 7.8147,
-    4: 100,
+    4: 13,
     5: 11.070,
     6: 12.592,
     7: 14.067,
