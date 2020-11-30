06b6e9cbda6d1abbec2e16ed24a6d7d79d606096
rezaa89 <reza.asadi68@gmail.com>
2020-11-29 22:40:23 -0800

    Update kalman_filter.py

--- deep_sort/deep_sort/kalman_filter.py
+++ deep_sort/deep_sort/kalman_filter.py
@@ -10,9 +10,9 @@ function and used as Mahalanobis gating threshold.
 """
 chi2inv95 = {
     1: 3.8415,
-    2: 19.21,
+    2: 50,
     3: 7.8147,
-    4: 60,
+    4: 70,
     5: 11.070,
     6: 12.592,
     7: 14.067,
@@ -39,7 +39,7 @@ class KalmanFilter(object):
 
     def __init__(self):
         ndim, dt = 4, 1.
-        print('chi value ', chi2inv95[2])
+        print('chi value ', chi2inv95[2], chi2inv95[4])
         # Create Kalman filter model matrices.
         self._motion_mat = np.eye(2 * ndim, 2 * ndim)
         for i in range(ndim):
